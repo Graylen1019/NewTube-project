308f9a136d1c83fcd80b8732e32060d4edecf946
Graylen <delilhabigelow@gmail.com>
2025-04-13 05:01:16 -0400

    ";)"

--- src/app/Home/page.tsx
+++
@@ -1,8 +0,0 @@
-const Page = () => {
-  console.log("where am i rendered");
-
-  const data = db.select().from(videos).where(eq(videos.id, "123"));
-  return <div>Feed Page!</div>;
-};
-
-export default Page;
