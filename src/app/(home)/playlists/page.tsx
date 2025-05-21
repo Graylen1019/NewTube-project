1482a4d5125f9dff4cd08edcf92c550e3241fdab
Graylen <delilhabigelow@gmail.com>
2025-05-21 03:13:49 -0400

    Implemented Custom playlist procedure and page, waiting on population.

---
+++ src/app/(home)/playlists/page.tsx
@@ -0,0 +1,16 @@
+import { DEFAULT_LIMIT } from "@/constants";
+import { PlaylistsView } from "@/modules/playlists/ui/views/playlist-view";
+import { HydrateClient, trpc } from "@/trpc/server";
+
+const Page = async () => {
+
+    void trpc.playlists.getMany.prefetchInfinite({ limit: DEFAULT_LIMIT})
+
+    return ( 
+        <HydrateClient>
+            <PlaylistsView />
+        </HydrateClient>
+    );
+}
+ 
+export default Page;
